58ce2a1a22232ce2c89b4e876481e2e65ff27e93
Tarund777 <tarundandugula1@gmail.com>
2025-01-29 22:22:35 +0530

    aef

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ server.listen(port,()=>{
 
 const io = new Server(server,{
   cors: {
-    origin: "http://localhost:5173", // Replace with your client URL
+    origin: "https://spacecc.onrender.com", // Replace with your client URL
     methods: ["GET", "POST"],
 },
 })
